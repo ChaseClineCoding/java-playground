3b3a53ad813f55b5d6db482e29b2bb9054638981
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-02-15 20:31:24 -0700

    Fixed gradle issue

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,7 +3,9 @@ package com.galvanize;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        System.out.println();
+
     }
 
 }
