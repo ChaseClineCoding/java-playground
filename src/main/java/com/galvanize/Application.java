1a9742328812a99c739f03d5b2b2857bac063a65
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-02-17 21:14:10 -0700

    Picture class with constructors

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -4,7 +4,8 @@ public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println();
+        Picture p = new Picture(10, 5);
+        System.out.println(p);
 
     }
 
