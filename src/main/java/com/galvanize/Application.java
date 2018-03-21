70cc48762dbb807cf8ffcecc50fd3aaa4606bc9d
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-03-20 20:12:23 -0600

    Using lombok for boilerplate code

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -4,6 +4,8 @@ public class Application {
 
     public static void main(String[] args) {
 
-
+        Container container = new Container();
+        container.setMaterial("plastic");
+        System.out.println(container);
     }
 }
---
+++ src/main/java/com/galvanize/Container.java
@@ -0,0 +1,12 @@
+package com.galvanize;
+
+import lombok.Data;
+
+@Data
+public class Container {
+
+    private String material;
+    private String manufacturer;
+    private float volume;
+
+}
