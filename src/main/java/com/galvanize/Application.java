bc3ccb8bbf7378a62f9a019ee9b63e849c759093
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-04-16 11:04:07 -0600

    Clean slate

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -4,8 +4,5 @@ public class Application {
 
     public static void main(String[] args) {
 
-        Container container = new Container();
-        container.setMaterial("plastic");
-        System.out.println(container);
     }
 }
