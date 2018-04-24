c6c937764353e53a783c0eea1e76c9d2ecdb3f70
Chase (Ryan Cline) <ryancline88@yahoo.com>
2018-04-24 16:02:27 -0600

    clean slate

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,20 +1,8 @@
 package com.galvanize;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Application {
 
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("\\w+\\s\\w+");
-        Matcher matcher = pattern.matcher("hello world");
-        boolean matches = matcher.matches();
 
-        System.out.println(matches);
-
-        Pattern pattern1 = Pattern.compile("\\W");
-        String string = String.join("", pattern1.split("a,b,c,d.d.c.b.a"));
-
-        System.out.println(string);
     }
 }
